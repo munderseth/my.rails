2717f2bab8aa987556d88c38db13b888f027d0ca
munderseth <marku@s2technologies.com>
2023-12-27 07:55:54 -0800

    archive before refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,9 +39,9 @@ class ArticlesController < ApplicationController
 
     redirect_to(root_path, status: :see_other)
   end
-  
+
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :status)
     end
 end
